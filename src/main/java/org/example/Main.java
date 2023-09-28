64da198a41785fddfee79b493d22a50347aa3ce1
FranciscoDíazRoldán <pacodiaz008@gmail.com>
2023-09-28 12:20:04 +0200

    Apuntes Persistencia a Objetos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
@@ -10,18 +11,23 @@ import java.util.Properties;
 public class Main {
     public static void main(String[] args) {
 
+        InputStream is = Main.class.getClassLoader().getResourceAsStream("database.properties");
+//También sirve         var is = Main.class.getClassLoader().getResourceAsStream("database.properties");
+
         Properties config = new Properties();
+
         try {
-            config.load(new FileReader("database.properties"));
+            //config.load(new FileReader("database.properties"));
+            config.load(is);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        config.put("version", "1.0");
+        /*config.put("version", "1.0");
         try {
             config.store(new FileWriter("database.properties"),"Añadida la version");
         } catch (IOException e) {
             throw new RuntimeException(e);
-        }
+        }*/
 
         System.out.println(config.get("url"));
         System.out.println(config.get("user"));
