4f8e4461302649ba9d7ebfeffea7e530f7679f42
FranciscoDíazRoldán <pacodiaz008@gmail.com>
2023-09-28 12:33:20 +0200

    Apuntes Persistencia a Objetos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,34 @@
 package org.example;
 
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
+import org.example.model.Alumno;
+
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Properties;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+
     public static void main(String[] args) {
+
+    Alumno fer = new Alumno();
+    fer.setNombre("Fernando");
+    fer.setApellido("Perez");
+
+    Alumno pablo = new Alumno();
+    pablo.setNombre("Pablo");
+    pablo.setApellido("Postigo");
+
+    var clase = new ArrayList<Alumno>();
+    clase.add(pablo);
+    clase.add(fer);
+    System.out.println(clase);
+
+    }
+
+
+    public static void main2(String[] args) {
 
         InputStream is = Main.class.getClassLoader().getResourceAsStream("database.properties");
 //También sirve         var is = Main.class.getClassLoader().getResourceAsStream("database.properties");
@@ -22,6 +41,14 @@ public class Main {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
+        //Para guardarlo como XML
+        try {
+            config.storeToXML(new FileOutputStream("datanase.properties"),"ok");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         /*config.put("version", "1.0");
         try {
             config.store(new FileWriter("database.properties"),"Añadida la version");
---
+++ src/main/java/org/example/model/Alumno.java
@@ -0,0 +1,12 @@
+package org.example.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+public class Alumno {
+    private String nombre;
+    private String apellido;
+
+
+}
